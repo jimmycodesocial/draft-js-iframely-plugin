a2d79c240c497d6812c6c24987a5aabc77cdf7e6
Alberto Menendez Romero <albertomr86@gmail.com>
2018-06-04 02:47:11 -0300

    Side toolbar integration

---
+++ src/components/Button/index.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class EmbedButton extends React.PureComponent {
+  onClick = (event) => {
+    event.preventDefault();
+
+    this.props.setEditorState(
+      this.props.addEmbeder(this.props.getEditorState())
+    );
+  };
+
+  onMouseDown = (event) => {
+    event.preventDefault();
+  }
+
+  blockTypeIsActive = () => {
+    const editorState = this.props.getEditorState();
+    const blockType = editorState
+      .getCurrentContent()
+      .getBlockForKey(editorState.getSelection().getStartKey())
+      .getType();
+
+    return blockType === this.props.entityType;
+  };
+
+  render() {
+    const { theme } = this.props;
+    const className = this.blockTypeIsActive()
+      ? `${theme.button} ${theme.active}`
+      : theme.button;
+
+    return (
+      <div className={theme.buttonWrapper} onMouseDown={this.onMouseDown}>
+        <button className={className} onClick={this.onClick} type="button">
+          <svg 
+            xmlns="http://www.w3.org/2000/svg"
+            version="1.1"
+            viewBox="0 0 30 30"
+            enableBackground="new 0 0 30 30"
+            className="svgIcon"
+            height="24"
+            width="24">
+            <polygon points="30,13.917 19,7 19,10 27,15 19,20 19,23 30,15.959" />
+            <polygon points="0,13.917 11,7 11,10 3,15 11,20 11,23 0,15.958" />
+          </svg>
+        </button>
+      </div>
+    );
+  }
+}
+
+EmbedButton.propTypes = {
+  theme: PropTypes.object,
+  entityType: PropTypes.string.isRequired,
+  getEditorState: PropTypes.func.isRequired,
+  setEditorState: PropTypes.func.isRequired,
+  addEmbeder: PropTypes.func.isRequired  
+};
+
+EmbedButton.defaultProps = {
+  theme: {},
+};
+
+export default EmbedButton;
