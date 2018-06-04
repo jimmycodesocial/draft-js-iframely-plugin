76423b4bd4922cc7162d4522b60bea624d5a4700
Alberto Menendez Romero <albertomr86@gmail.com>
2018-06-04 02:44:41 -0300

    Bundle the plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,54 @@
 /* eslint-disable no-var */
 var path = require('path');
+var nodeExternals = require('webpack-node-externals');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  externals: [nodeExternals({
+    modulesFromFile: true
+  })],
+  mode: 'production',
+  optimization: {
+    minimize: true
+  },
+  performance: {
+    hints: false
+  },
   output: {
-    publicPath: '/',
-    libraryTarget: 'commonjs2', // necessary for the babel plugin
-    path: path.join(__dirname, 'lib-css'), // where to place webpack files
+    libraryTarget: 'commonjs2',
+    path: path.join(__dirname, 'lib'),
+    filename: 'index.js'
   },
   module: {
-    loaders: [
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: { 
+          loader: 'babel-loader', 
+          options: { 
+            plugins: ['transform-runtime'],
+            presets: [
+              'es2015',
+              'react',
+              'stage-0'
+            ]
+          } 
+        }
+      },
       {
         test: /\.css$/,
-        loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader?modules&importLoaders=1&localIdentName=draftJsIframelyPlugin__[local]__[hash:base64:5]!postcss-loader' }),
-      },
-    ],
+        use: ExtractTextPlugin.extract({
+          use: 'css-loader?modules&importLoaders=1&localIdentName=draftJsIframelyPlugin__[local]__[hash:base64:5]!postcss-loader'
+        })
+      }
+    ]
   },
 
   plugins: [
-    new ExtractTextPlugin({ filename: `${path.parse(process.argv[2]).name}.css` }),
-  ],
+    new ExtractTextPlugin('plugin.css')
+  ]
 };
