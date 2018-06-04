c6bf5d89b96fd0d28c9850b16cc79da06f1f182c
Alberto Menendez Romero <albertomr86@gmail.com>
2018-06-04 02:46:55 -0300

    Embeder

---
+++ src/components/Embeder/index.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Embeder extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = { text: '' }; 
+  }
+
+  componentDidMount() {
+    this.input.focus();
+  }
+
+  cancel = () => {
+    this.props.blockProps.onCancel(this.props.block);
+    this.props.blockProps.setReadOnly(false);
+  }
+
+  onChange = (event) => this.setState({ text: event.target.value });
+  
+  onFocus = (event) => this.props.blockProps.setReadOnly(true);
+
+  onKeyDown = (event) => {
+    // Cancel on Escape or Del.
+    if ((event.keyCode === 27) || (event.keyCode === 46 && this.state.text.length === 0)) {
+      this.cancel();
+    }
+  }
+
+  onKeyPress = async (event) => {
+    if (event.key === 'Enter') {
+      await this.props.blockProps.onRequest(this.props.block, this.state.text);
+      this.props.blockProps.setReadOnly(false);
+    }
+  }
+
+  render() {
+    const { blockProps, theme } = this.props;
+    const { placeholder } = blockProps;
+        
+    return (
+      <div
+        className={theme.embederContainer}
+        onBlur={this.cancel}
+        onFocus={this.onFocus} >
+
+        <input 
+          ref={ref => this.input = ref}
+          className={theme.embederInput}
+          placeholder={placeholder}
+          value={this.state.text}
+          onChange={this.onChange}
+          onKeyDown={this.onKeyDown} 
+          onKeyPress={this.onKeyPress}/>
+      </div>
+    );
+  }
+}
+
+Embeder.propTypes = {
+  block: PropTypes.object,
+  blockProps: PropTypes.shape({
+    placeholder: PropTypes.string.isRequired,
+    setReadOnly: PropTypes.func.isRequired,
+    onCancel: PropTypes.func.isRequired,
+    onRequest: PropTypes.func.isRequired
+  })
+};
+
+export default Embeder;
